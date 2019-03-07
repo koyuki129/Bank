623adc86d0d8d1794b3d6a8df74d5186c2d67376
Koyuki129 <salehi88@hotmail.com>
2019-03-07 13:48:21 +0100

    localhost 3000
    localhost 3000

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const app = express();
 // from the www folder
 app.use(express.static('www'));
 // Start the web server on port 3000
-app.listen(3000,() => console.log('Listening on port 3000'));
+app.listen(3000,() => console.log('Listening on localhost:3000'));
