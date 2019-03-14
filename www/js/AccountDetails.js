5f320ca7eede6de43ce93288bc03f0c7f87ad3a3
Shilpa <shilpadahiya87@gmail.com>
2019-03-14 17:05:10 +0100

    function to show 10 more transactions

--- www/js/AccountDetails.js
+++ www/js/AccountDetails.js
@@ -1,17 +1,52 @@
 class AccountDetails {
 
   constructor(){
+    $(document).on('click', '#show-button', e => this.showMore(e));
+    this.numberOfTransactions = 10;
   }
-
-  updateDisplay() {
+  
+  showMore() {
     if (!App.user) { return; }
-    $('#accountname').text(this.accountName);
+    this.numberOfTransactions+=10;
+    
     let html = '';
     // loop through the logged in users accounts and create html
     for (let account of App.user.accounts) {
 
       if (account.name == this.accountName) {
-        for (let history of account.history) {
+        
+        if (this.numberOfTransactions > account.history.length) {
+          this.numberOfTransactions = account.history.length;
+        }
+
+        for (let i = 0; i < this.numberOfTransactions ; i++) {
+          let history = account.history[i];
+          html += `<tr>
+              <th scope="row">${history.label}</th>
+              <td>${history.amount}</td>
+              <td class="text-right">${history.time}</td>
+          </tr>`;
+        }
+      }
+      
+
+    }
+    // put the html in the DOM
+    $('.history tbody').html(html);
+  }
+
+
+  updateDisplay() {
+    if (!App.user) { return; }
+    $('#accountname').text(this.accountName);
+    this.numberOfTransactions = 10;
+    let html = '';
+    // loop through the logged in users accounts and create html
+    for (let account of App.user.accounts) {
+
+      if (account.name == this.accountName) {
+        for (let i = 0; i < 10; i++) {
+          let history = account.history[i];
           html += `<tr>
               <th scope="row">${history.label}</th>
               <td>${history.amount}</td>
