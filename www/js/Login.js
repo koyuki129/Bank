7e5c69262cd1a7fc29bdedd4d905c245be70e83e
Unknown <ninosandr@gmail.com>
2019-03-21 15:30:07 +0100

    Page och refresh
    Nu huvudsidan är start istället mina konto och vi stannar kvar på samma sidan om vi trycker refresh.

--- www/js/Login.js
+++ www/js/Login.js
@@ -31,7 +31,9 @@ class Login extends RegisterUser {
       $('.only-if-not-logged-in').hide();
       $('.username').text(App.user.username);
       // Goto the my accounts page
-      location.hash = '#my-accounts';
+      if (location.hash == '#login') {
+        location.hash = '#start';
+      }
       // Save the user file name in session storage as well
       // (so we can keep being login after hard page reloads)
       window.sessionStorage.userfile = this.file2;
